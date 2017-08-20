44741b341e671cf60ad93d03e8a8b81d53c8b539
Lasse Folkersen <FOLK@lundbeck.com>
2017-08-20 18:15:01 +0200

    try 12

--- analyze/ui.R
+++ analyze/ui.R
@@ -18,48 +18,13 @@ shinyUI(bootstrapPage(
     uiOutput("ui")
     
   ),
-  # 
-  # conditionalPanel(
-  #   condition = "input.trait_group == 'all'",
-  #   selectInput("trait_all", "Traits:", choices = selections_all)
-  # ),
-  # conditionalPanel(
-  #   condition = "input.trait_group == 'disease'",
-  #   selectInput("trait_disease", "Traits:", choices = selections_disease)
-  # ),
-  # conditionalPanel(
-  #   condition = "input.trait_group == 'biometrics'",
-  #   selectInput("trait_biometrics", "Traits:", choices = selections_biometrics)
-  # ),
-  # conditionalPanel(
-  #   condition = "input.trait_group == 'biomarker'",
-  #   selectInput("trait_biomarker", "Traits:", choices = selections_biomarker)
-  # ),
-  # conditionalPanel(
-  #   condition = "input.trait_group == 'response'",
-  #   selectInput("trait_response", "Traits:", choices = selections_response)
-  # ),
-  # conditionalPanel(
-  #   condition = "input.trait_group == 'other'",
-  #   selectInput("trait_other", "Traits:", choices = selections_other)
-  # ),
-  # 
-  # checkboxInput("advanced", label ="Advanced options", value = FALSE),
-  # conditionalPanel(
-  #   condition = "input.advanced",
-  #   radioButtons("trait_group", "Trait categories:", trait_groups, selected = "all"),
-  #   radioButtons("ethnicity_group", label="Reference population:", choices=ethnicities, selected = "global", inline = FALSE,width = NULL),
-  #   checkboxInput("real_dist", label ="Plot real distribution (experimental)", value = FALSE)
-  #   
-  # ),
-  # 
   actionButton("goButton","Run analysis"),
   endPanel(),
   beginPanel('2/3'),
   
-  # h2("Genetic risk score:"),
+  h2("Genetic risk score:"),
   # htmlOutput("text_1"),
-  # plotOutput("plot_1"),
+  plotOutput("plot1"),
   # htmlOutput("text_2"),
   # dataTableOutput("table1"),
   # htmlOutput("text_3"),
