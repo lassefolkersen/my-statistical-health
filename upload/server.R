60cf71bcfd67d2861bea99a1a01fc05e906d3abc
Lasse Folkersen <FOLK@lundbeck.com>
2017-08-20 17:58:14 +0200

    try 7

--- upload/server.R
+++ upload/server.R
@@ -8,16 +8,23 @@ source("/home/ubuntu/srv/my-statistical-health/functions.R")
 # Define server logic for random distribution application
 shinyServer(function(input, output) {
   output$ui <- renderUI({
-    uniqueID<-isolate(gsub(" ","",input$uniqueID))
+    uniqueID <- gsub(" ","",input$uniqueID)
+    blank <- checkboxGroupInput("dynamic", "Dynamic",choices = "none")
     if(nchar(uniqueID)!=12 | length(grep("^id_",uniqueID))==0){
-      out <- checkboxGroupInput("dynamic", "Dynamic",choices = "none")
-      stop(safeError("E1"))
+      return(blank)
+      
     }else{
-      load(paste0("/home/ubuntu/data/",uniqueID,"/",uniqueID,".rdata"))
-      out <- checkboxGroupInput("dynamic", "Dynamic",choices = colnames(d))
-      stop(safeError("E2"))
+      f1 <- paste0("/home/ubuntu/data/",uniqueID,"/",uniqueID,".rdata")
+      if(!file.exists(f1)){
+        return(blank)
+      }else{
+        load(f1)  
+        out <-checkboxGroupInput("dynamic", "Dynamic",choices = colnames(d))
+        return(out)
+        
+      }
+
     }
-    return(out)
   })
 })
 
