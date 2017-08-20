80df03eac411b362075afa5b6509d1e295e76403
Lasse Folkersen <FOLK@lundbeck.com>
2017-08-20 17:12:21 +0200

    bug 2

--- upload/ui.R
+++ upload/ui.R
@@ -9,8 +9,8 @@ shinyUI(bootstrapPage(
 	beginPage(),
 	beginPanel('1/3'),
 	fileInput("largeFile", "Upload genome data", multiple = FALSE, accept = NULL),
-	textInput(inputId="email", label = "Email", value = "nacho@24genetics.com"),
-	HTML("<u><a href='http://www.impute.me/www/terms_of_use.html'>Terms of use.</a></u>"),
+	textInput(inputId="email", label = "Email", value = "lassefolkersen@gmail.com"),
+	HTML("<u><a href='http://www.my-statistical-health.com/www/terms_of_use.html'>Terms of use.</a></u>"),
 	checkboxInput("delete2weeks", "Delete data after two weeks", value = TRUE, width = NULL),
 	actionButton("goButton","Send data to analysis"),
 	
@@ -22,9 +22,9 @@ shinyUI(bootstrapPage(
 	beginPanel('2/3'),
 	HTML("Upload a file containing the health data. It must excel xlsx format, the first column must be dates, and the first row must be headers. You can download an <a href='www.my-statistical-health.com/www/template.xlsx'>example template here</a>."),
 	
-	# textOutput("text1"),
-	# textOutput("text2"),
-	# htmlOutput("text3"),
+	
+	textOutput("text2"),
+	htmlOutput("text3"),
 	endPanel(),
 			
 				
