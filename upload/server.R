580230d85e4df990098334554b0115608464082a
Lasse Folkersen <FOLK@lundbeck.com>
2017-08-20 17:48:09 +0200

    s

--- upload/server.R
+++ upload/server.R
@@ -1,25 +1,21 @@
 library("shiny")
 
-
 options(shiny.maxRequestSize=100*1024^2) 
-
 source("/home/ubuntu/srv/my-statistical-health/functions.R")
-
-
 
 
 
 # Define server logic for random distribution application
 shinyServer(function(input, output) {
-	
-	
   output$ui <- renderUI({
     uniqueID<-isolate(gsub(" ","",input$uniqueID))
     if(nchar(uniqueID)!=12 | length(grep("^id_",uniqueID))==0){
       checkboxGroupInput("dynamic", "Dynamic",choices = "none")
+      stop(safeError("E1"))
     }else{
       load(paste0("/home/ubuntu/data/",uniqueID,"/",uniqueID,".rdata"))
       out<-checkboxGroupInput("dynamic", "Dynamic",choices = colnames(d))
+      stop(safeError("E2"))
     }
     return(out)
   })
